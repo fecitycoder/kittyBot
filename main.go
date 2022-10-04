649d96e4eea696b2520a04ace1f3943534511de8
Artem Artyukhov <fecity.coder@gmail.com>
2022-10-04 09:56:30 +0300

    Changed some features. In order to optimize the code, I pass values by reference.

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 
 func main() {
 	progSettings := filereader.SettingsRead("settings.ini")
-	botapi.InitBot(progSettings["botTocken"])
-
-	messages, _ := botapi.GetUpdate()
+	tocken := progSettings["botTocken"]
+	botapi.InitBot(&tocken)
+	var id int
+	messages := botapi.GetUpdate(&id)
 	//last := strings.ToLower(messages[len(messages)-1])
 	for _, mes := range messages {
-		fmt.Println(mes)
+		fmt.Println(mes, id)
 	}
 }
