9e3aab71610715d541ea6a9f9ffc00f84660326e
Artem Artyukhov <fecity.coder@gmail.com>
2022-10-13 16:28:04 +0300

    Промежуточный результат, перед отделением функции в отдельную библиотеку.

--- pkg/weatherapi/weatherapi.go
+++ pkg/weatherapi/weatherapi.go
@@ -1,7 +1,5 @@
 package weatherapi
 
-import "net/http"
-
 const weatherBaseUrl = "https://api.weather.yandex.ru/v2/informers"
 
 var weatherKey string
@@ -10,21 +8,6 @@ func InitWehather(key string) {
 	weatherKey = key
 }
 
-func getBodyByUrl(){
-    client := &http
-	.Client{} 
-    req, err := http.NewRequest( 
-         "GET", "https://google.com", nil, 
-    ) 
-    // добавляем заголовки
-    req.Header.Add("Accept", "text/html")   // добавляем заголовок Accept
-    req.Header.Add("User-Agent", "MSIE/15.0")   // добавляем заголовок User-Agent
-  
-    resp, err := client.Do(req) 
-    if err != nil { 
-        fmt.Println(err) 
-        return
-    } 
-    defer resp.Body.Close() 
-    io.Copy(os.Stdout, resp.Body)
+func getBodyByUrl() {
+
 }
