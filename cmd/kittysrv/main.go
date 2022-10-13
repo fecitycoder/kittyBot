79ad10289425bc9ca1f21f4c7433ff6b6f1f36f5
Artem Artyukhov <fecity.coder@gmail.com>
2022-10-13 16:28:04 +0300

    Промежуточный результат, перед отделением функции в отдельную библиотеку.

--- cmd/kittysrv/main.go
+++ cmd/kittysrv/main.go
@@ -2,18 +2,24 @@ package main
 
 // Импортирум модули
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"mykittybot/pkg/botapi"
 	"mykittybot/pkg/filereader"
+	"mykittybot/pkg/weatherapi"
+	"net/http"
 )
 
 func main() {
 	progSettings := filereader.SettingsRead("../../settings.ini")
 	botapi.InitBot(progSettings["botTocken"])
+	weatherapi.InitWehather(progSettings["weatherKey"])
 	messageQuerry := new(botapi.GetUpdatesT)
 	messageQuerry.GetUpdate()
 
 	var lastMessages []string //
+	weather := weatherapi.WeatherQueryT{}
 
 	for _, messmessage := range messageQuerry.Result {
 		lastMessages = append(lastMessages, fmt.Sprintf("%v", messmessage.Message.Text))
@@ -21,4 +27,22 @@ func main() {
 		botapi.ClearQueue()
 	}
 
+	client := &http.Client{}
+	req, err := http.NewRequest(
+		"GET", "https://api.weather.yandex.ru/v2/informers?lat=52.339204&lon=35.350873", nil,
+	)
+
+	req.Header.Add("X-Yandex-API-Key", progSettings["weatherKey"])
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(body, &weather)
+
+	fmt.Println(weather.Fact.Temp)
 }
