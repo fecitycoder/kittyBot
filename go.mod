feaebb18847a841522efc070fe7c80545efc9c45
Artem Artyukhov <fecity.coder@gmail.com>
2022-12-02 10:09:35 +0300

    q

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module mykittybot
 
 go 1.18
-
-require github.com/ekomobile/dadata/v2 v2.7.0 // indirect
--- pkg/weatherapi/weatherapi.go
+++ pkg/weatherapi/weatherapi.go
@@ -3,7 +3,7 @@ package weatherapi
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
@@ -34,7 +34,7 @@ func getBodyByUrl(url string) []byte {
 
 	defer response.Body.Close()
 
-	body, err := ioutil.ReadAll(response.Body)
+	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
