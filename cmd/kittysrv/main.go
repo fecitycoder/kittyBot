f7f1a51784440d27ee8b925e77e67b5eb56c1927
fecity.coder <fecity.coder@gmail.com>
2024-03-12 16:30:12 +0300

    Полностью вывел библиотеки по работе с API в отдельные модули

--- cmd/kittysrv/main.go
+++ cmd/kittysrv/main.go
@@ -3,10 +3,11 @@ package main
 // Импортирум модули
 import (
 	"fmt"
-	kitty "mykittybot/pkg/botapi"
-	"mykittybot/pkg/filereader"
-	"mykittybot/pkg/weatherapi"
-    "strings"
+	"strings"
+
+	kitty "github.com/fecitycoder/botapi"
+	"github.com/fecitycoder/filereader"
+	"github.com/fecitycoder/weatherapi"
 )
 
 func main() {
@@ -17,10 +18,10 @@ func main() {
 	var lastMessages []string // Пул последних сообщений бота
 	var ids []int
 
-	messageQuerry := kitty.GetUpdatesT{} // Структура для хранения структуры сообщений чата телеграм
+	messageQuerry := kitty.GetUpdatesT{}  // Структура для хранения структуры сообщений чата телеграм
 	weather := weatherapi.WeatherQueryT{} // Структура для хранения данных о погоде
 
-    for {
+	for {
 		messageQuerry.GetUpdate()
 		for _, messmessage := range messageQuerry.Result {
 			lastMessages = append(lastMessages, fmt.Sprintf("%v", messmessage.Message.Text))
@@ -28,28 +29,27 @@ func main() {
 			kitty.ClearQueue()
 		}
 		for n, message := range lastMessages {
-            search := strings.ToLower(message)
-            if search == "картинка" {
+			search := strings.ToLower(message)
+			if search == "картинка" {
 				kitty.SendPhotoByIrl(&ids[n], "https://www.ixbt.com/img/n1/news/2019/5/3/chrome-73-mode-sombre-android_large.jpg")
 			}
-            if search == "погода"{
-                chat := &ids[n]
-                weather.GetWeather()
-                headerText := fmt.Sprintf("Погода в г.Железногорск")
-                conditionText := fmt.Sprintf("на улице %s", weatherapi.Condition[weather.Fact.Condition])
+			if search == "погода" {
+				chat := &ids[n]
+				weather.GetWeather()
+				headerText := fmt.Sprintf("Погода в г.Железногорск")
+				conditionText := fmt.Sprintf("на улице %s", weatherapi.Condition[weather.Fact.Condition])
 
-                fmt.Println (weather)
+				fmt.Println(weather)
 
-                    //fmt.Sprintf("Температура воздуха составляет: %s °C", cond[weather.Fact.Conditions])
-                kitty.SendMessage(chat, headerText)
-                kitty.SendMessage(chat, conditionText)
+				//fmt.Sprintf("Температура воздуха составляет: %s °C", cond[weather.Fact.Conditions])
+				kitty.SendMessage(chat, headerText)
+				kitty.SendMessage(chat, conditionText)
 
-
-            }
-            if search == "stop" || search == "стоп"{
+			}
+			if search == "stop" || search == "стоп" {
 				return
 			}
 			lastMessages = nil
 		}
-            	}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
-module mykittybot
+module github.com/fecitycoder/kittyBot
 
-go 1.18
+go 1.22.1
+
+require (
+	github.com/fecitycoder/botapi v0.0.0-20240312132018-e76f6580ebfb
+	github.com/fecitycoder/filereader v0.0.0-20240312132644-1416e9c3e30d
+	github.com/fecitycoder/weatherapi v1.0.0
+)
